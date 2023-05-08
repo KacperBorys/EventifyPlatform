04776e80b7b7f0665455daff12ab97b8930883a3
mstrojek@student.agh.edu.pl <mstrojek@student.agh.edu.pl>
2023-05-08 20:05:42 +0200

    Dodanie person

--- EventsProject/Models/Classes/Person.cs
+++ EventsProject/Models/Classes/Person.cs
@@ -7,5 +7,19 @@ namespace EventsProject.Models.Classes
 {
     public class Person
     {
+        public Person(string firstName, string lastName, string email, string phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            Phone = phone;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+
     }
 }
