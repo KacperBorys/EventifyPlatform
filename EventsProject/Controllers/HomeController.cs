9aba9045cd13952f6915334e69ccaf7eff307efa
Adrian Żyła <adrianzyla2@gmail.com>
2023-05-23 21:24:38 +0200

    poprawiony landing page

--- EventsProject/Controllers/HomeController.cs
+++ EventsProject/Controllers/HomeController.cs
@@ -11,26 +11,51 @@ namespace EventsProject.Controllers
 {
     public class HomeController : Controller
     {
+        // <summary>
+        /// Displays the index view.
+        /// </summary>
+        /// <returns>The index view</returns>
+        [HttpGet]
         public ActionResult Index()
         {
+            List<Event> listOfEventsFromDB;
+            using (DatabaseContext db = new DatabaseContext())
+            {
+                listOfEventsFromDB = db.Events.ToList();
+                EventsProject.Controllers.EventController.eventsList = listOfEventsFromDB;
+            }
             return View();
         }
-
+        /// <summary>
+        /// Displays the about view.
+        /// </summary>
+        /// <returns>The about view</returns>
         public ActionResult About()
         {
             return View();
         }
-
+        /// <summary>
+        /// Displays the create event view.
+        /// </summary>
+        /// <returns>The create event view</returns>
         public ActionResult CreateEvent()
         {
             return View();
         }
-
+        /// <summary>
+        /// Displays the profile view.
+        /// </summary>
+        /// <returns>The profile view</returns>
         public ActionResult Profile()
         {
             return View();
         }
 
+        /// <summary>
+        /// Displays the event information view for a specific event.
+        /// </summary>
+        /// <param name="nameOfEvent">The name of the event</param>
+        /// <returns>The event information view</returns>
         public ActionResult Event_Informations(string nameOfEvent)
         {
             Event eventInfo = EventsProject.Controllers.EventController.eventsList.FirstOrDefault(e => e.EventName == nameOfEvent);
@@ -41,6 +66,11 @@ namespace EventsProject.Controllers
             return RedirectToAction("Events", "Home");
         }
 
+        /// <summary>
+        /// Displays the event modification view for a specific event.
+        /// </summary>
+        /// <param name="nameOfEvent">The name of the event</param>
+        /// <returns>The event modification view</returns>
         [HttpGet]
         public ActionResult EventModified(string nameOfEvent)
         {
@@ -51,11 +81,20 @@ namespace EventsProject.Controllers
             }
             return View(newEvent);
         }
+
+        /// <summary>
+        /// Displays the shopping cart view.
+        /// </summary>
+        /// <returns>The shopping cart view</returns>
         public ActionResult Shopping_cart()
         {
             return View();
         }
 
+        /// <summary>
+        /// Displays the events view and fetches the list of events from the database.
+        /// </summary>
+        /// <returns>The events view</returns>
         [HttpGet]
         public ActionResult Events()
         {
@@ -67,9 +106,6 @@ namespace EventsProject.Controllers
             }
             return View();
         }
-        public ActionResult EventModifcation()
-        {
-            return View();
-        }
+
     }
 }
